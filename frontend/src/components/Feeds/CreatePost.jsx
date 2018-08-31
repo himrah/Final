06915dd05bbff80fdc0efbe68d4f901e03d2b786
himrah <rkrjob55@gmail.com>
2018-08-31 11:51:49 +0530

    update

--- frontend/src/components/Feeds/CreatePost.jsx
+++ frontend/src/components/Feeds/CreatePost.jsx
@@ -13,25 +13,39 @@ class CreatePost extends Component {
                 <h4>Create Post</h4>
                 </div>
           </header>
-          <div className="postform">
-            <form id="createpost">
-                <textarea className="form-control" placeholder="write something" name="post"/>
-            </form>
-        </div>
-        <div className="_post_icon">
-            <div className="fl_rw">
-                <div className="top_p">
-                    <img src={image} className="alogo" alt="image" />
+            <div className="postform">
+                <form id="createpost">
+                    <textarea className="form-control" placeholder="write something" name="post"/>
+                </form>
                 </div>
-                <div className="top_p">
-                    <img src={video} className="alogo" alt="video" />
+
+            <div className="user-post">
+                <div className="_p">
+                    
+
                 </div>
-                
-                <div className="top_p">
-                <img src={attach} className="alogo" alt="attachment" />
-                </div>
-             </div>   
-        </div>
+            
+            </div>
+
+
+
+            <div className="_post_icon">
+                <div className="fl_rw">
+                    <div className="top_p">
+                        <label for="input-image">
+                        <img src={image} className="alogo" alt="image"/>
+                        </label>
+                        <input type="file" id="file_input" id="input-image" accept=".jpg, .jpeg, .png, .mp4" multiple="multiple"/>
+                    </div>
+                    <div className="top_p">
+                        <img src={video} className="alogo" alt="video" />
+                    </div>
+                    
+                    <div className="top_p">
+                    <img src={attach} className="alogo" alt="attachment" />
+                    </div>
+                </div>   
+            </div>
 
       </article>
     )
